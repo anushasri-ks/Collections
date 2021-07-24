129dc2bf0f2ff301de5307a92af349614243a67f
Anushasri KS <83582662+anushasri-ks@users.noreply.github.com>
2021-07-25 00:29:30 +0530

    Fish example

--- src/com/xworkz/functional/tester/FishDTOTester.java
+++ src/com/xworkz/functional/tester/FishDTOTester.java
@@ -19,16 +19,16 @@ public class FishDTOTester {
 		list.add(dto1);
 		list.add(dto2);
 
-//		sorting name in ascending order
-		Comparator<FishDTO> fishName = new Comparator<FishDTO>() {
+//		sorting nameAsc in ascending order
+		Comparator<FishDTO> name = new Comparator<FishDTO>() {
 			@Override
 			public int compare(FishDTO o1, FishDTO o2) {
-				System.out.println("---name sorting asc---");
+				System.out.println("sorting nameAsc in ascending order");
 				return o1.getName().compareTo(o2.getName());
 			}
 		};
 
-		Collections.sort(list, fishName);
+		Collections.sort(list, nameAsc);
 
 		Iterator<FishDTO> itrName1 = list.iterator();
 		while (itrName1.hasNext()) {
@@ -37,16 +37,15 @@ public class FishDTOTester {
 		}
 		
 //		sorting name in descending order
-		Comparator<FishDTO> fishNameDesc = new Comparator<FishDTO>() {
+		Comparator<FishDTO> nameDesc = new Comparator<FishDTO>() {
 			@Override
 			public int compare(FishDTO o1, FishDTO o2) {
-				System.out.println("\n---name sorting desc---");
+				System.out.println("\nsorting name in descending order");
 				return o2.getName().compareTo(o1.getName());
 			}
-
 		};
 
-		Collections.sort(list, fishNameDesc);
+		Collections.sort(list, nameDesc);
 
 		Iterator<FishDTO> itrName2 = list.iterator();
 		while (itrName2.hasNext()) {
@@ -55,15 +54,15 @@ public class FishDTOTester {
 		}
 
 //		sorting color in ascending order
-		Comparator<FishDTO> fishColor = new Comparator<FishDTO>() {
+		Comparator<FishDTO> colorAsc = new Comparator<FishDTO>() {
 			@Override
 			public int compare(FishDTO o1, FishDTO o2) {
-				System.out.println("\n---color sorting asc---");
+				System.out.println("\nsorting color in ascending order");
 				return o1.getColor().compareTo(o2.getColor());
 			}
 		};
 
-		Collections.sort(list, fishColor);
+		Collections.sort(list, colorAsc);
 
 		Iterator<FishDTO> itrColor = list.iterator();
 		while (itrColor.hasNext()) {
@@ -75,7 +74,7 @@ public class FishDTOTester {
 		Comparator<FishDTO> lifeSpanAsc = new Comparator<FishDTO>() {
 			@Override
 			public int compare(FishDTO o1, FishDTO o2) {
-				System.out.println("\n---lifespan sorting asc---");
+				System.out.println("\nsorting lifeSpan in ascending order");
 				if (o1.getLifeSpan() == o2.getLifeSpan())
 					return 0;
 				if (o1.getLifeSpan() > o2.getLifeSpan())
@@ -99,7 +98,7 @@ public class FishDTOTester {
 		Comparator<FishDTO> lifeSpanDesc = new Comparator<FishDTO>() {
 			@Override
 			public int compare(FishDTO o1, FishDTO o2) {
-				System.out.println("\n---lifeSpan sorting desc---");
+				System.out.println("\nsorting lifeSpan in descending order");
 				if (o1.getLifeSpan() == o2.getLifeSpan())
 					return 0;
 				if (o1.getLifeSpan() > o2.getLifeSpan())
@@ -120,10 +119,10 @@ public class FishDTOTester {
 		}
 
 //		sorting cost in descending order
-		Comparator<FishDTO> cost = new Comparator<FishDTO>() {
+		Comparator<FishDTO> costDesc = new Comparator<FishDTO>() {
 			@Override
 			public int compare(FishDTO o1, FishDTO o2) {
-				System.out.println("\n---cost sorting desc---");
+				System.out.println("\nsorting cost in descending order");
 				if (o1.getCost() == o2.getCost())
 					return 0;
 				if (o1.getCost() > o2.getCost())
@@ -135,7 +134,7 @@ public class FishDTOTester {
 			}
 		};
 
-		Collections.sort(list, cost);
+		Collections.sort(list, costDesc);
 	
 		Iterator<FishDTO> itrCost = list.iterator();
 		while(itrCost.hasNext()) {
