5b2328f22e4ff9aea9c56c9d827f169c5155bcc8
Anushasri KS <83582662+anushasri-ks@users.noreply.github.com>
2021-08-15 22:03:03 +0530

    jdbc uploaded

--- src/com/xworkz/webseries/jdbc/constant/OTTPlatform.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz.webseries.jdbc.constant;
-
-public enum OTTPlatform {
-	
-	AmazonPrime, Netflix, Zee5, Hotstar, SunNxt, Voot;
-}
--- src/com/xworkz/webseries/jdbc/constant/genreType.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz.webseries.jdbc.constant;
-
-public enum genreType {
-
-	comedy, action, family, drama, thriller, horror, scifi, romance, mystery;
-}
